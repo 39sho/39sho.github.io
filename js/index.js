2865d9b536d9df3c6b6b2a1e0fe159328a330d8e
unknown <g.39sho@gmail.com>
2018-12-22 09:56:40 +0900

    make it better

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
-var imglist;
 fetch('img/wp_index.txt')
   .then((response) => response.text())
   .then((text) => {
-    imglist = text.split(`\n`);
+    var imglist = text.split(`\n`);
     var selectnum = Math.floor(Math.random() * imglist.length);
-    document.body.style.backgroundImage = `url(img/wallpapers/${imglist[selectnum]})`;
+    var wp = imglist[selectnum].replace(/ /g, '%20');
+    console.log(wp);
+    document.body.style.backgroundImage = `url(img/wallpapers/${wp})`;
   })
   .catch((err) => console.log(err));
