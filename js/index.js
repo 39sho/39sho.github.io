d770810188fe7f1ee383dda0eb325484d735c5a9
unknown <g.39sho@gmail.com>
2018-12-22 10:09:23 +0900

    make it better

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ fetch('img/wp_index.txt')
   .then((text) => {
     var imglist = text.split(`\n`);
     var selectnum = Math.floor(Math.random() * imglist.length);
-    var wp = imglist[selectnum].replace(/ /g, '%20').replace(/\(/g, '\(').replace(/\)/g, '\)');
+    var wp = imglist[selectnum].replace(/ /g, '%20').replace(/\(/g, '%28').replace(/\)/g, '%29');
     console.log(wp);
     document.body.style.backgroundImage = `url(img/wallpapers/${wp})`;
   })
