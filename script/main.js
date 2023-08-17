e85926692892fffb3a35634ed9ef21d28d69c438
39sho <g.39sho@gmail.com>
2023-08-17 14:22:29 +0900

    init vite

--- script/main.js
+++
@@ -1,4 +0,0 @@
-const time_element = document.querySelector("#last_modified");
-const last_modified = new Date(document.lastModified);
-time_element.datetime = last_modified.toISOString();
-time_element.textContent = new Intl.DateTimeFormat().format(last_modified);
